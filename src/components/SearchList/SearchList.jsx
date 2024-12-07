4551b000f32943c8d4942452e2165d2f92d033ff
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-12-07 16:36:16 +0700

    Add function filter lastName.

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -1,16 +1,29 @@
 import { SearchRounded } from "@mui/icons-material";
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SearchList = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const params = new URLSearchParams(location.search);
+    params.set("search", e.target.elements.search.value);
+    navigate(`${location.pathname}?${params}`);
+  };
   return (
-    <div className="flex w-[98%] items-center p-[6px] gap-2 rounded-[999px] border border-gray-300">
+    <form
+      onSubmit={handleSubmit}
+      className="flex w-[98%] items-center p-[6px] gap-2 rounded-[999px] border border-gray-300"
+    >
       <SearchRounded className="text-base text-[gray]" />
       <input
         className="border-none text-[12px] bg-transparent focus:outline-none placeholder:text-[gray]"
         type="text"
         placeholder="Search..."
+        name="search"
       />
-    </div>
+    </form>
   );
 };
 
