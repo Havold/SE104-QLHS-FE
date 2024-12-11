33c2f705a4d42437a98b971297cf95cb682fe4cc
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-12-12 01:01:48 +0700

    Fixed bug: Can't submit form when input is '1'.

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -1,29 +1,40 @@
 import { SearchRounded } from "@mui/icons-material";
-import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 const SearchList = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [inputSearch, setInputSearch] = useState(
+    searchParams.get("search") ? searchParams.get("search") : ""
+  );
   const handleSubmit = (e) => {
     e.preventDefault();
     const params = new URLSearchParams(location.search);
-    params.set("search", e.target.elements.search.value);
-    navigate(`${location.pathname}?${params}`);
+    params.set("search", inputSearch);
+    navigate(`${location.pathname}?${params}`, { replace: true });
   };
+
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex w-[98%] items-center p-[6px] gap-2 rounded-[999px] border border-gray-300"
-    >
+    <div className="flex w-[98%] items-center p-[6px] gap-2 rounded-[999px] border border-gray-300">
       <SearchRounded className="text-base text-[gray]" />
       <input
         className="w-full border-none text-[12px] bg-transparent focus:outline-none placeholder:text-[gray]"
         type="text"
         placeholder="Search..."
         name="search"
+        value={inputSearch}
+        onChange={(e) => {
+          setInputSearch(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSubmit(e);
+          }
+        }}
       />
-    </form>
+    </div>
   );
 };
 
