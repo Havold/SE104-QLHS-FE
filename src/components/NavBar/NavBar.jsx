36b564be110e2da64c3567a1ffc7db01f1a0053e
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-12-16 21:56:21 +0700

    Fetch user data in NavBar.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,37 @@
 // import "./navBar.scss";
+import { useContext } from "react";
 import Search from "../Search/Search";
 import { ReviewsOutlined, CampaignOutlined } from "@mui/icons-material";
+import { AuthContext } from "../../context/authContext";
 
 const NavBar = () => {
-  const avatar ='https://i.pinimg.com/564x/97/bb/06/97bb067e30ff6b89f4fbb7b9141025ca.jpg'
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="flex items-center justify-between p-2 sticky top-0 bg-white custom-box-shadow z-50">
-        <Search />
+      <Search />
       <div className="flex items-center gap-12 w-full justify-end ">
-          <ReviewsOutlined fontSize="small" className="cursor-pointer text-[gray]" />
-          <div className="relative cursor-pointer">
-            <CampaignOutlined fontSize="small" className="text-[gray] " />
-            <div className="w-4 h-4 flex items-center justify-center absolute top-0 -right-2 bg-[#7d38f3] text-[white] rounded-full text-[10px]"><span>1</span></div>
+        <ReviewsOutlined
+          fontSize="small"
+          className="cursor-pointer text-[gray]"
+        />
+        <div className="relative cursor-pointer">
+          <CampaignOutlined fontSize="small" className="text-[gray] " />
+          <div className="w-4 h-4 flex items-center justify-center absolute top-0 -right-2 bg-[#7d38f3] text-[white] rounded-full text-[10px]">
+            <span>1</span>
           </div>
+        </div>
         <div className="flex items-center gap-2 cursor-pointer">
           <div className="flex flex-col items-end">
-            <h3 className="text-[12px]">John Doe</h3>
-            <div className="text-[10px] text-[gray]">Admin</div>
+            <h3 className="text-[12px]">{currentUser.fullName}</h3>
+            <div className="text-[10px] text-[gray]">
+              {currentUser.role.name}
+            </div>
           </div>
-            <img className="w-[40px] h-[40px] rounded-full object-cover" src={avatar} alt="profilePic" />
+          <img
+            className="w-[40px] h-[40px] rounded-full object-cover"
+            src={currentUser.img ? currentUser.img : "/assets/noAvatar.jpg"}
+            alt="profilePic"
+          />
         </div>
       </div>
     </div>
