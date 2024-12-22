ed1ef1b1e9a580f7abf6e050b49bf35d5aff1dd9
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-12-22 16:54:52 +0700

    Remove type select of input tag.

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -13,7 +13,11 @@ const InputField = ({
       </label>
 
       {type === "select" ? (
-        <select className="text-[12px] p-2 h-[40px] border border-gray-400 outline-webSkyBold caret-webSkyBold transition-colors rounded-md">
+        <select
+          className="text-[12px] p-2 h-[40px] border border-gray-400 outline-webSkyBold caret-webSkyBold transition-colors rounded-md"
+          defaultValue={options[0]}
+          {...register(name)}
+        >
           {options.map((option, index) => (
             <option key={index} value={option}>
               {option}
