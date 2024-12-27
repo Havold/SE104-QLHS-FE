81e2088d27922cedc727cd713a2453f3826c61de
Trương Hữu Thọ <96384986+Havold@users.noreply.github.com>
2024-12-28 00:01:03 +0700

    Add method DELETE: remove a student from a specific score board.

--- src/pages/detailScoreBoard/DetailScoreBoard.jsx
+++ src/pages/detailScoreBoard/DetailScoreBoard.jsx
@@ -46,64 +46,68 @@ const columns = [
   },
 ];
 
-const renderRows = (data) => {
-  return data ? (
-    data.map((item) => (
-      <tr
-        className="text-sm border-b-2 border-gray-100 even:bg-slate-100 hover:bg-webPurpleLight "
-        key={item.student.id}
-      >
-        <td className="flex items-center gap-4 p-4">
-          <img
-            className="md:hidden lg:block w-8 h-8 rounded-full object-cover"
-            src={
-              item.student.img
-                ? `${process.env.REACT_APP_API_URL}${item.student.img}`
-                : "/assets/noAvatar.jpg"
-            }
-            alt="profilePic"
-          />
-          <div className="flex flex-col">
-            <h2 className="text-[12px] font-semibold">{`${item.student.fullName}`}</h2>
-            {/* <span className="text-[8px] text-gray-500">
-              {item.studentClasses[0]?.classSchoolYear.class.name}
-            </span> */}
-          </div>
-        </td>
-        <td className="hidden lg:table-cell">{item.student.id}</td>
-
-        <td className="hidden md:table-cell">{item.student.phone}</td>
-        <td className="table-cell">
-          <input
-            className="w-8 h-8 text-center text-[12px] rounded-md border-2 outline-webSkyBold border-gray-500"
-            defaultValue={item.score}
-            type="text"
-          />
-        </td>
-        <td>
-          <div className="flex gap-4">
-            <Link to={`/list/students/${item.id}`}>
-              <button className="flex w-8 h-8 rounded-full bg-webSky items-center justify-center">
-                <VisibilityOutlined
-                  style={{ fontSize: 16, color: "whitesmoke" }}
-                />
-              </button>
-            </Link>
-            {role === "admin" ? (
-              <FormModal type="remove" table="students-class" id={item.id} />
-            ) : (
-              <></>
-            )}
-          </div>
-        </td>
-      </tr>
-    ))
-  ) : (
-    <></>
-  );
-};
-
 const DetailScoreBoard = () => {
+  const renderRows = (data) => {
+    return data ? (
+      data.map((item) => (
+        <tr
+          className="text-sm border-b-2 border-gray-100 even:bg-slate-100 hover:bg-webPurpleLight "
+          key={item.student.id}
+        >
+          <td className="flex items-center gap-4 p-4">
+            <img
+              className="md:hidden lg:block w-8 h-8 rounded-full object-cover"
+              src={
+                item.student.img
+                  ? `${process.env.REACT_APP_API_URL}${item.student.img}`
+                  : "/assets/noAvatar.jpg"
+              }
+              alt="profilePic"
+            />
+            <div className="flex flex-col">
+              <h2 className="text-[12px] font-semibold">{`${item.student.fullName}`}</h2>
+              {/* <span className="text-[8px] text-gray-500">
+                {item.studentClasses[0]?.classSchoolYear.class.name}
+              </span> */}
+            </div>
+          </td>
+          <td className="hidden lg:table-cell">{item.student.id}</td>
+
+          <td className="hidden md:table-cell">{item.student.phone}</td>
+          <td className="table-cell">
+            <input
+              className="w-8 h-8 text-center text-[12px] rounded-md border-2 outline-webSkyBold border-gray-500"
+              defaultValue={item.score}
+              type="text"
+            />
+          </td>
+          <td>
+            <div className="flex gap-4">
+              <Link to={`/list/students/${item.id}`}>
+                <button className="flex w-8 h-8 rounded-full bg-webSky items-center justify-center">
+                  <VisibilityOutlined
+                    style={{ fontSize: 16, color: "whitesmoke" }}
+                  />
+                </button>
+              </Link>
+              {role === "admin" ? (
+                <FormModal
+                  type="remove"
+                  table={`detail-score-boards/${scoreBoardId}/students`}
+                  id={item.student.id}
+                />
+              ) : (
+                <></>
+              )}
+            </div>
+          </td>
+        </tr>
+      ))
+    ) : (
+      <></>
+    );
+  };
+
   const [searchParams, setSearchParams] = useSearchParams();
   let page = searchParams.get("page") ? parseInt(searchParams.get("page")) : 1;
   let pageItems = searchParams.get("pageItems")
